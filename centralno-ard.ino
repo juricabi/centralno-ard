3465c5381706d9e534a18b9dfab11ddaf2f8584a
jb.justcode <darkhors1@gmail.com>
2022-01-16 14:50:26 +0100

    2022. dorade

--- centralno-ard.ino
+++ centralno-ard.ino
@@ -1,7 +1,8 @@
+//Centralno, Jurica Bilicic
 
-//Pametna kuca, Jurica Bilicic
-
+#include "Arduino.h"
 #define DHT_PIN 10
+
 #include <dht.h>
 #include <LiquidCrystal_I2C.h>
 #include <Wire.h>
@@ -24,6 +25,7 @@ LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);//definiraj pinove
 
 
 float namtemp = 55, tmpnamtemp = 0, tempkot = 0, tempkotold = 25 , tempboj = 0, tempbojold = 25;
+int maxtemp = 80, mintemp = 20;
 unsigned long previousMillis = 0, greske = 0;
 short pumpa = 0, brojacispis = 1, brojacpumpa = 0;
 
@@ -32,24 +34,95 @@ void setup()
 {
 
 	Serial.begin(115200);//Brzina prijenosa bluetooth modula
-	pinMode(3, INPUT);
-	pinMode(4, INPUT);
+  pinMode(3, INPUT_PULLUP);
+  pinMode(4, INPUT_PULLUP);
 	pinMode(2, OUTPUT);
 	digitalWrite(2, HIGH);
 	sensor1.begin();
-  sensor2.begin();
+  sensor2.begin(); 
 	sensor1.setResolution(DS18B20n1, 10);
   sensor2.setResolution(DS18B20n2, 10);
   
 	EEPROM.get(0, tmpnamtemp);
 
-	if (tmpnamtemp >= 20 && tmpnamtemp <= 120)
+	if (tmpnamtemp >= mintemp && tmpnamtemp <= maxtemp)
 		namtemp = tmpnamtemp;
 
 	lcd.begin(16, 2);
 	lcd.off();
 	lcd.begin(16, 2);
 
+}
+
+void kontrola_gumbi() {
+
+
+  // Kontrola gumbi
+    if (digitalRead(3) == LOW) {
+
+      if (namtemp >= maxtemp)
+      {
+        lcd.setCursor(0, 0);
+        namtemp = maxtemp;
+        EEPROM.put(0, namtemp);
+        lcd.print("                ");
+        lcd.setCursor(0, 0);
+        lcd.print("Postavi nize!");
+      }
+
+      else
+      {
+        namtemp += 0.5;
+        EEPROM.put(0, namtemp);
+        lcd.print("                ");
+        lcd.setCursor(0, 0);
+        lcd.print("Nam.Temp ");
+        lcd.print(namtemp, 1);
+        lcd.write(223);
+        lcd.print("C");
+      }
+
+    }
+
+    else if (digitalRead(4) == LOW) {
+
+      if (namtemp <= mintemp)
+      {
+        lcd.setCursor(0, 0);
+        namtemp = mintemp;
+        EEPROM.put(0, namtemp);
+        lcd.print("                ");
+        lcd.setCursor(0, 0);
+        lcd.print("Postavi vise!");
+      }
+
+      else
+      {
+        namtemp -= 0.5;
+        EEPROM.put(0, namtemp);
+        lcd.print("                ");
+        lcd.setCursor(0, 0);
+        lcd.print("Nam.Temp ");
+        lcd.print(namtemp, 1);
+        lcd.write(223);
+        lcd.print("C");
+
+      }
+
+
+    }
+
+    else 
+    {
+      lcd.setCursor(0, 0);
+      lcd.print("Nam.Temp ");
+      lcd.print(namtemp, 1);
+      lcd.write(223);
+      lcd.print("C");
+    }
+ // kraj kontrola sa gumbima
+ 
+  
 }
 
 
@@ -65,10 +138,10 @@ void loop() //Program se izvr�ava u beskona�noj petlji.
 
 		case 10:
 		{
-			if (namtemp >= 120)
+			if (namtemp >= maxtemp)
 			{
 				Serial.print("Namjestena temperatura prevelika!\n");
-				namtemp = 120;
+				namtemp = maxtemp;
 				EEPROM.put(0, namtemp);
 			}
 
@@ -86,9 +159,9 @@ void loop() //Program se izvr�ava u beskona�noj petlji.
 		case 11:
 		{
 
-			if (namtemp <= 20) {
+			if (namtemp <= mintemp) {
 				Serial.print("Namjestena temperatura preniska!\n");
-				namtemp = 20;
+				namtemp = mintemp;
 				EEPROM.put(0, namtemp);
 			}
 
@@ -148,6 +221,7 @@ void loop() //Program se izvr�ava u beskona�noj petlji.
 
 	}
 
+ // osvježavanje zaslona
 	if ((unsigned long)(millis() - previousMillis) >= 1200)  //jednostavni timer za svaku sekundu
 
 	{
@@ -247,83 +321,16 @@ void loop() //Program se izvr�ava u beskona�noj petlji.
 			brojacispis = 0;
 		}
 
-
-
-		if ((digitalRead(3) == HIGH)) {
-
-			if (namtemp >= 120)
-			{
-				lcd.setCursor(0, 0);
-				namtemp = 120;
-				EEPROM.put(0, namtemp);
-				lcd.print("                ");
-				lcd.setCursor(0, 0);
-				lcd.print("Postavi nize!");
-			}
-
-			else
-			{
-				namtemp += 0.5;
-				EEPROM.put(0, namtemp);
-				lcd.print("                ");
-				lcd.setCursor(0, 0);
-				lcd.print("Nam.Temp ");
-				lcd.print(namtemp, 1);
-				lcd.write(223);
-				lcd.print("C");
-			}
-
-		}
-
-		else if ((digitalRead(4) == HIGH)) {
-
-			if (namtemp <= 20)
-			{
-				lcd.setCursor(0, 0);
-				namtemp = 20;
-				EEPROM.put(0, namtemp);
-				lcd.print("                ");
-				lcd.setCursor(0, 0);
-				lcd.print("Postavi vise!");
-			}
-
-			else
-			{
-				namtemp -= 0.5;
-				EEPROM.put(0, namtemp);
-				lcd.print("                ");
-				lcd.setCursor(0, 0);
-				lcd.print("Nam.Temp ");
-				lcd.print(namtemp, 1);
-				lcd.write(223);
-				lcd.print("C");
-
-			}
-
-
-		}
-
-		else 
-		{
-			lcd.setCursor(0, 0);
-			lcd.print("Nam.Temp ");
-			lcd.print(namtemp, 1);
-			lcd.write(223);
-			lcd.print("C");
-		}
-
-
-
 		if (tempkot >= namtemp && brojacpumpa == 0)
 		{
-			digitalWrite(2, LOW);
+			digitalWrite(2, HIGH);
 			pumpa = 1;
 			brojacpumpa = 1;
 		}
 
 		else if (tempkot < namtemp && brojacpumpa == 0)
 		{
-			digitalWrite(2, HIGH);
+			digitalWrite(2, LOW);
 			pumpa = 0;
 			brojacpumpa = 1;
 		}
@@ -336,6 +343,8 @@ void loop() //Program se izvr�ava u beskona�noj petlji.
 		else {
 			brojacpumpa = 0;
 		}
+
+    kontrola_gumbi();
 
 	}
 
